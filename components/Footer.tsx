e59793b7eb7ade0b975a6a18464a8f35ecb6812c
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2021-09-19 00:16:56 +0200

    feat: footer icons, flex draft

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,32 +1,56 @@
 import React from "react";
 import styles from "../styles/Footer.module.css";
+import Image from "next/image";
 
 export default function Footer() {
 	return (
 		<div className={styles.globalFooter}>
-			<div className={styles.medexLogo}>Medex</div>
 			<div className={styles.searchBoxLeft}>
-				<span className={styles.smallText}>
-					What are you looking for?
-				</span>
+				<h2>Medex</h2>
+				<p className={styles.smallText}>What are you looking for?</p>
 				<input type="text" />
 			</div>
 			<div className={styles.learnInfo}>
 				<h3>Learn</h3>
-				<p>About us</p>
-				<p>Our Experts</p>
-				<p>How it works</p>
-				<p>Purpose</p>
-				<p>Investors</p>
+				<p className={styles.linksUnderline}>About us</p>
+				<p className={styles.linksUnderline}>Our Experts</p>
+				<p className={styles.linksUnderline}>How it works</p>
+				<p className={styles.linksUnderline}>Purpose</p>
+				<p className={styles.linksUnderline}>Investors</p>
+				<p className={styles.linksUnderline}>Reviews</p>
 			</div>
-			<div className={styles.ContactInfo}>
+			<div className={styles.contactInfo}>
 				<h3>Contact</h3>
-				<p>Support</p>
-				<p>contact@medex.com</p>
+				<p className={styles.linksUnderline}>Support</p>
+				<p className={styles.linksUnderline}>contact@medex.com</p>
 				<div className={styles.smallIcons}>
-					<p>Instagram icon</p>
-					<p>Facebook icon</p>
-					<p>Twitter icon</p>
+					<p className={styles.oneIcon}>
+						<Image
+							src="/icons/instagram.png"
+							alt="Free Medical Advice"
+							width={36}
+							height={36}
+							layout="fixed"
+						/>
+					</p>
+					<p className={styles.oneIcon}>
+						<Image
+							src="/icons/facebook.png"
+							alt="Free Medical Advice"
+							width={36}
+							height={36}
+							layout="fixed"
+						/>
+					</p>
+					<p className={styles.oneIcon}>
+						<Image
+							src="/icons/twitter.png"
+							alt="Free Medical Advice"
+							width={36}
+							height={36}
+							layout="fixed"
+						/>
+					</p>
 				</div>
 			</div>
 		</div>
