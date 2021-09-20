df9b049de5162599606e9ca2da9b4c461b9b3a0d
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2021-09-20 23:45:06 +0200

    refactor: for use jsx <b> tags

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import styles from "../styles/FAQ.module.css";
 import Image from "next/image";
 
-function FaqOneQuestion(props: any) {
+function CloseIcon(props: any) {
 	return (
-		<div className={styles.faqBox}>
+		<div>
 			<span className={styles.closeIcon}>
 				<Image
 					src="/icons/faqClose.png"
@@ -15,8 +15,6 @@ function FaqOneQuestion(props: any) {
 					className={styles.iconClass}
 				/>
 			</span>
-			<h3>{props.question}</h3>
-			<h4>{props.answer}</h4>
 		</div>
 	);
 }
@@ -26,44 +24,76 @@ export default function FAQ() {
 		<div className={styles.globalFaq} id="faq">
 			<h2>Frequently Asked Questions</h2>
 			<div className={styles.faqArray}>
-				<FaqOneQuestion
-					question={
-						"How long does it take to hear back from a medical doctor?"
-					}
-					answer={
-						"You should hear back from a healthcare provider on the Medex platform in a few minutes after request. If your request was sent at night, then the response time may increase to serval hours of waiting"
-					}
-				/>
-				<FaqOneQuestion
-					question={"Who is Medex for?"}
-					answer={
-						"Medex is a digital health service that provides telemedicine and telepharmacy for adults to treat their health problems quickly and effectively at any time and any place."
-					}
-				/>
-				<FaqOneQuestion
-					question={"Who is the health care professionals in Medex?"}
-					answer={
-						"All of the healthcare team on the Medex platform are licensed healthcare professionals. Each doctor undergoeas an extensive background check and license verification process. When you message or talk with a physican on the Medex platform you can see their resume and medical license."
-					}
-				/>
-				<FaqOneQuestion
-					question={"Can I use any pharmacy?"}
-					answer={
-						"Yes, you can just choose the address of your local pharmacy and medications will be shipped there for free."
-					}
-				/>
-				<FaqOneQuestion
-					question={"Can I purchase medications with home delivery?"}
-					answer={
-						"OTC medications: <b> Yes</b>, Rx medications: No, The current position of the Chief Pharmaceutical Inspector is identical to the act and is clear - internet - order sale of prescription drugs is impossible. Pharmaceutical law regulations in force in Poland do not allow this."
-					}
-				/>
-				<FaqOneQuestion
-					question={"Is my personal data safe?"}
-					answer={
-						"Yes. We take your privacy and the security of your information very seriously. Please read our Privacy Policy for more details. "
-					}
-				/>
+				<div className={styles.faqBox}>
+					<CloseIcon />
+					<h3>
+						How long does it take to hear back from a medical
+						doctor?
+					</h3>
+					<h4>
+						You should hear back from a healthcare provider on the
+						Medex platform in a few minutes after request. If your
+						request was sent at night, then the response time may
+						increase to serval hours of waiting
+					</h4>
+				</div>
+
+				<div className={styles.faqBox}>
+					<CloseIcon />
+					<h3>Who is Medex for?</h3>
+					<h4>
+						Medex is a digital health service that provides
+						telemedicine and telepharmacy for adults to treat their
+						health problems quickly and effectively at any time and
+						any place.
+					</h4>
+				</div>
+
+				<div className={styles.faqBox}>
+					<CloseIcon />
+					<h3>Who is the health care professionals in Medex?</h3>
+					<h4>
+						All of the healthcare team on the Medex platform are
+						licensed healthcare professionals. Each doctor
+						undergoeas an extensive background check and license
+						verification process. When you message or talk with a
+						physican on the Medex platform you can see their resume
+						and medical license.
+					</h4>
+				</div>
+
+				<div className={styles.faqBox}>
+					<CloseIcon />
+					<h3>Can I use any pharmacy?</h3>
+					<h4>
+						Yes, you can just choose the address of your local
+						pharmacy and medications will be shipped there for free.
+					</h4>
+				</div>
+
+				<div className={styles.faqBox}>
+					<CloseIcon />
+					<h3>Can I purchase medications with home delivery?</h3>
+					<h4>
+						<b>OTC</b> medications: <b> Yes</b>,<br />
+						<b>Rx</b> medications: <b>No</b>, <br />
+						The current position of the Chief Pharmaceutical
+						Inspector is identical to the act and is clear -
+						internet - order sale of prescription drugs is
+						impossible. Pharmaceutical law regulations in force in
+						Poland do not allow this.
+					</h4>
+				</div>
+
+				<div className={styles.faqBox}>
+					<CloseIcon />
+					<h3>Is my personal data safe?</h3>
+					<h4>
+						Yes. We take your privacy and the security of your
+						information very seriously. Please read our Privacy
+						Policy for more details.{" "}
+					</h4>
+				</div>
 			</div>
 		</div>
 	);
