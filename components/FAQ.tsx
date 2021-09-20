fcbf7ef257a9b97dd53335e85a5f02323023a63c
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2021-09-20 22:45:48 +0200

    refactor: remove component, all in one file

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -1,6 +1,25 @@
 import React from "react";
 import styles from "../styles/FAQ.module.css";
-import FaqOneQuestion from "./elements/FaqOneQuestion";
+import Image from "next/image";
+
+function FaqOneQuestion(props: any) {
+	return (
+		<div className={styles.faqBox}>
+			<span className={styles.closeIcon}>
+				<Image
+					src="/icons/faqClose.png"
+					alt="Free Medical Advice"
+					width={25}
+					height={25}
+					layout="fixed"
+					className={styles.iconClass}
+				/>
+			</span>
+			<h3>{props.question}</h3>
+			<h4>{props.answer}</h4>
+		</div>
+	);
+}
 
 export default function FAQ() {
 	return (
